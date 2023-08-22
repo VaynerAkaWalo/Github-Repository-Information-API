3a2385ec9d591fe3fa06e412f75ff33ee61fba78
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-08-22 18:53:34 +0200

    This should be interface from beginning.

--- src/main/java/com/example/githubapiproxy/proxy/GithubAPIProxy.java
+++ src/main/java/com/example/githubapiproxy/proxy/GithubAPIProxy.java
@@ -2,7 +2,7 @@ package com.example.githubapiproxy.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "GithubProxy", url = "${github.api.url}")
-public class GithubAPIProxy {
 
+@FeignClient(name = "GithubProxy", url = "${github.api.url}")
+public interface GithubAPIProxy {
 }
