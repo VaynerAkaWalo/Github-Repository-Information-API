74bed2a761060a6f908492b387e6bf1eddb93764
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-08-22 18:55:02 +0200

    Fetching user repositories and branch info

--- src/main/java/com/example/githubapiproxy/proxy/GithubAPIProxy.java
+++ src/main/java/com/example/githubapiproxy/proxy/GithubAPIProxy.java
@@ -1,8 +1,20 @@
 package com.example.githubapiproxy.proxy;
 
+import com.example.githubapiproxy.model.github.Branch;
+import com.example.githubapiproxy.model.github.RepositoriesPage;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 
 @FeignClient(name = "GithubProxy", url = "${github.api.url}")
 public interface GithubAPIProxy {
+
+    @GetMapping("/search/repositories?q=user:{username}&page={page}&per_page=100")
+    RepositoriesPage fetchUserRepositories(@PathVariable String username, @PathVariable int page);
+
+    @GetMapping("/repos/{repository}/branches")
+    List<Branch> fetchBranchInfo(@PathVariable String repository);
 }
