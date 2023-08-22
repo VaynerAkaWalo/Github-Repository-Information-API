e043b6eac4443f205d3f6fcab768f6b0d23bc685
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-08-22 18:47:42 +0200

    GithubProxy basic setup

---
+++ src/main/java/com/example/githubapiproxy/proxy/GithubAPIProxy.java
@@ -0,0 +1,8 @@
+package com.example.githubapiproxy.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "GithubProxy", url = "${github.api.url}")
+public class GithubAPIProxy {
+
+}
